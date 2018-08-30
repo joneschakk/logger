f446963012975c0df86215f02870e32a8acb7a2f
joneschakk <27142447+joneschakk@users.noreply.github.com>
2018-08-30 14:47:19 +0530

    Update lib_logrotate.cpp

--- lib_logrotate.cpp
+++ lib_logrotate.cpp
@@ -201,10 +201,7 @@ public:
         environment,
         None(),
         parentHooks);
-    for (int i=10; i>0; --i) {
-    std::cout << i << std::endl;
-    std::this_thread::sleep_for (std::chrono::seconds(5));
-  }
+   
 
     if (outProcess.isError()) {
       os::close(outfds.write.get());
